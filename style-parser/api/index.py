ba3967cf5495442904879e9f3de10e6f11b5fcb6
Алексей Невротов <elazarsound@gmail.com>
2025-06-20 10:02:12 +0300

    app

--- style-parser/api/index.py
+++ style-parser/api/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-app = Flask(__name__, static_folder='../static', template_folder='../templates')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route("/", methods=["GET", "POST"])
 def index():
