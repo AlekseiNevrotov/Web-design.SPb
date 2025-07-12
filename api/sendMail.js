ce475d297c0fec73a70246e72f0e93ed521fb2ae
Алексей Невротов <elazarsound@gmail.com>
2025-07-12 18:29:04 +0300

    phone

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,10 +1,9 @@
 import nodemailer from 'nodemailer';
-
 export async function POST(request) {
   try {
     const {
       name,
-      email,
+      phone,
       message,
       pages_count,
       design_selected,
@@ -12,11 +11,9 @@ export async function POST(request) {
       cms_selected,
       total_cost
     } = await request.json();
-
-    if (!name || !email || !message) {
+    if (!name || !phone || !message) {
       return new Response(JSON.stringify({ error: 'Заполните все поля' }), { status: 400 });
     }
-
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -24,8 +21,6 @@ export async function POST(request) {
         pass: process.env.GMAIL_APP_PASS,
       },
     });
-
-    // Формируем массив строк только с непустыми данными калькулятора
     const calcFields = [
       pages_count ? `Количество страниц: ${pages_count}` : null,
       design_selected ? `Дизайн: ${design_selected}` : null,
@@ -33,22 +28,19 @@ export async function POST(request) {
       cms_selected ? `CMS: ${cms_selected}` : null,
       total_cost ? `Итоговая стоимость: ${total_cost}` : null,
     ].filter(Boolean);
-
     const mailText =
       `Имя: ${name}\n` +
-      `Email: ${email}\n` +
+      `Email: ${phone}\n` +
       `Сообщение:\n${message}\n\n` +
       (calcFields.length
         ? `---\nДанные калькулятора:\n${calcFields.join('\n')}\n`
         : '');
-
     await transporter.sendMail({
       from: process.env.GMAIL_USER,
       to: process.env.GMAIL_USER,
       subject: `Новое сообщение от ${name}`,
       text: mailText,
     });
-
     return new Response(JSON.stringify({ message: 'Письмо отправлено' }), { status: 200 });
   } catch (error) {
     console.error(error);
