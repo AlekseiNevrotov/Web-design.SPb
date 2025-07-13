567a57c9dca729b1d404985b183cd4e85fd58af1
Алексей Невротов <elazarsound@gmail.com>
2025-07-13 14:12:40 +0300

    send

--- api/sendMail.js
+++ api/sendMail.js
@@ -12,9 +12,9 @@ export async function POST(request) {
       total_cost
     } = await request.json();
     if (!phone) {
-  return new Response(JSON.stringify({ error: 'Укажите номер телефона' }), { status: 400 });
-}
-const phoneRegex = /^\d{10}$/;
+      return new Response(JSON.stringify({ error: 'Укажите номер телефона' }), { status: 400 });
+    }
+    const phoneRegex = /^\d{10}$/;
 if (!phoneRegex.test(phone)) {
   return new Response(JSON.stringify({ error: 'Номер телефона введён некорректно (10 цифр).' }), { status: 400 });
 }
