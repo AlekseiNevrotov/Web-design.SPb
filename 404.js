89807e9b067a10871b78194bbe513351db86b248
Алексей Невротов <elazarsound@gmail.com>
2025-08-04 09:48:20 +0300

    404

---
+++ 404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Custom404 = () => {
+    return (
+        <div style={{ textAlign: 'center', padding: '50px' }}>
+            <h1>404 - Страница не найдена</h1>
+            <p>Извините, мы не можем найти то, что вы ищете.</p>
+            <a href="/">Вернуться на главную</a>
+        </div>
+    );
+};
+
+export default Custom404;
